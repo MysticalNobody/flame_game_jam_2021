354a6d53bdd8a6314e8ea82c464b01eb18f5c443
Anton Malofeev <anton@xsoulspace.dev>
2021-10-30 12:30:05 +0300

    fix: candy bag

--- lib/entites/candy_bag_entity.dart
+++ lib/entites/candy_bag_entity.dart
@@ -1,1 +1,3 @@
 part of entities;
+
+class CandyBagEntity {}
--- lib/systems/candy_bag_system.dart
+++ lib/systems/candy_bag_system.dart
@@ -1,1 +1,31 @@
 part of systems;
+
+class CandyBagSystem extends System {
+  void createBagEntity() {
+    // createEntity(
+    //     name: 'Entity $i',
+    //     position: size / 2,
+    //     size: Vector2.all(64),
+    //     angle: 0,
+    //   )
+    //     ..add<SpriteComponent, SpriteInit>(
+    //       SpriteInit(await loadSprite('pizza.png')),
+    //     )
+    //     ..add<VelocityComponent, Vector2>(
+    //       Vector2(
+    //         random.nextDouble() * 100 * (random.nextBool() ? 1 : -1),
+    //         random.nextDouble() * 100 * (random.nextBool() ? 1 : -1),
+    //       ),
+    //     );
+  }
+
+  @override
+  void execute(double delta) {
+    // TODO: implement execute
+  }
+
+  @override
+  void init() {
+    // TODO: implement init
+  }
+}
