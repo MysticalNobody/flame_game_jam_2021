64d5f0b8592c1b8b4b1a0423eb7e7799c654c594
Kit <micronanobot.ver3@gmail.com>
2021-10-31 21:45:45 +0300

    add change audio

--- lib/presentation/game/game_view.dart
+++ lib/presentation/game/game_view.dart
@@ -34,21 +34,25 @@ class AppGameView extends StatefulWidget {
 final audios = List.generate(5, (index) => 'bg_${index + 1}.mp3');
 
 class _AppGameViewState extends State<AppGameView> {
+  int num = 0;
   @override
   void initState() {
     FlameAudio.audioCache.fixedPlayer = AudioPlayer();
-    play(0);
+    FlameAudio.audioCache.fixedPlayer!.stop();
+    FlameAudio.audioCache.fixedPlayer!.onPlayerCompletion.listen((_) {
+      if (num + 1 < audios.length) {
+        play(num + 1);
+      } else {
+        play(0);
+      }
+    });
+    play(num);
     super.initState();
   }
 
   Future<void> play(int num) async {
     await FlameAudio.playLongAudio(audios[num]);
-    await FlameAudio.audioCache.fixedPlayer?.onPlayerCompletion.single;
-    if (num + 1 < audios.length) {
-      play(num + 1);
-    } else {
-      play(0);
-    }
+    log(FlameAudio.audioCache.fixedPlayer!.state.toString());
   }
 
   @override
